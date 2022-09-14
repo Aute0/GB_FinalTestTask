f57c4301bdbfc43e2b60fc6ac261c7061d336a80
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-09-14 16:42:13 +0300

    add readme.md and prog repo

---
+++ prog/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Массив можно ввести с клавиатуры, либо
+// задать на старте алгоритма.
+
+string[] ArrayFirst = new string[6] { "2h", "p9", "59830", "23df", "res", "sonya" };
+string[] ArrayLength = new string[ArrayFirst.Length];
+void ArraySolution(string[] ArrayFirst, string[] ArrayLength)
+
+{
+    int count = 0;
+    for (int i = 0; i < ArrayFirst.Length; i++)
+    {
+        if (ArrayFirst[i].Length <= 3)
+        {
+            ArrayLength[count] = ArrayFirst[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySolution(ArrayFirst, ArrayLength);
+PrintArray(ArrayLength);
+
